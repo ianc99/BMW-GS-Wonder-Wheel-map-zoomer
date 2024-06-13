264971d419e6289ad4e7b41d7119309f5d1992d2
ianc99 <31339933+ianc99@users.noreply.github.com>
2024-06-13 14:57:08 +0100

    Add files via upload

---
+++ code/CAN_Read_plus_Keyboard_V0.5.ino
@@ -0,0 +1,320 @@
+// 
+//
+#define US_KEYBOARD 1
+
+#include <Arduino.h>
+#include "BLEDevice.h"
+#include "BLEHIDDevice.h"
+#include "HIDTypes.h"
+#include "HIDKeyboardTypes.h"
+#include <mcp_can.h>  // you need MCP_CAN library by Cory Fowler https://github.com/coryjfowler/MCP_CAN_lib
+#include <SPI.h>
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+bool WonderWheelMoved = false;
+
+#define DEVICE_NAME "ESP32 Keyboard"
+
+// Forward declarations
+void bluetoothTask(void*);
+void typeText(const char* text);
+void(* resetFunc) (void) = 0;
+
+bool isBleConnected = false;
+
+//Pins below moved from default to ease board design
+#define CAN0_INT 25 // 19 default 15
+
+#define SPI_SCK 26  // 18 default 18
+#define SPI_MOSI 27  // 05 default 23
+#define SPI_MISO 14 // 17 default 19
+#define SPI_CS 12   // 16 default 05
+
+
+MCP_CAN CAN0(SPI_CS);                               
+
+#define CENTRE 0xFC
+#define LEFT 0xFD
+#define RIGHT 0xFE
+
+
+char CurrentWheelRotation;  //Rotation position
+char LastWheelRotation;
+char CurrentWheelDirection; //Left or Right
+char LastWheelDirection;
+
+void setup()
+{
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS);
+  Serial.begin(115200);
+  Serial.println("Setup Start");
+  xTaskCreate(bluetoothTask, "bluetooth", 20000, NULL, 5, NULL);
+
+  // Initialize MCP2515 running at 8MHz (check your crystal for this value) with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
+    Serial.println("MCP2515 Initialized Successfully!");
+  else
+    Serial.println("Error Initializing MCP2515...");
+  
+  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+
+  pinMode(CAN0_INT, INPUT);                     // Configuring pin for /INT input
+  
+  Serial.println(__FILE__);
+
+CurrentWheelDirection=CENTRE;
+LastWheelDirection=CENTRE;
+CurrentWheelRotation=0x00;
+LastWheelRotation=0x00;
+
+}
+
+void loop()
+{
+  if(!digitalRead(CAN0_INT) && isBleConnected)                         // If CAN0_INT pin is low and BT connected, read receive buffer
+  {
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    if (rxId == 0x2A0) // The ID for Left hand controls.  See https://docs.google.com/spreadsheets/d/1tUrOES5fQZa92Robr6uP8v2dzQDq9ohHjUiTU3isqdc/edit#gid=615500728
+        {
+        CurrentWheelRotation = rxBuf[5]; // 5th byte for rotation
+        CurrentWheelDirection = rxBuf[3];  //3rd byte for side to side movement
+        if (CurrentWheelRotation > LastWheelRotation)
+          {
+            sprintf(msgString,"Wheel turned to 0x%.2X ", CurrentWheelRotation);
+            typeText("+");
+            LastWheelRotation= CurrentWheelRotation;
+            WonderWheelMoved = true;
+          }
+          else if (CurrentWheelRotation < LastWheelRotation )
+          {
+            sprintf(msgString,"Wheel turned to 0x%.2X ", CurrentWheelRotation);
+            typeText("-");
+            LastWheelRotation= CurrentWheelRotation;
+            WonderWheelMoved = true;            
+          }
+        if (CurrentWheelDirection != LastWheelDirection )
+          {
+              switch (CurrentWheelDirection) {
+                case LEFT:
+                  sprintf(msgString,"Wheel moved from Centre to Left");
+                  typeText("C");
+                  break;
+                case RIGHT:
+                  sprintf(msgString,"Wheel moved from Centre to Left");
+                  typeText("D");                
+                  break;
+                case CENTRE:
+                  sprintf(msgString,"Wheel returned to centre");
+                 // typeText("C");
+                  break;
+                default:
+                  sprintf(msgString,"Wheel in unknown position");
+                  break;
+              }
+            LastWheelDirection = CurrentWheelDirection;
+            WonderWheelMoved = true;
+          }
+          if (WonderWheelMoved)
+            {
+            Serial.println(msgString);
+            WonderWheelMoved=false;
+            }
+        }
+  }
+}
+
+
+// Everything below here is for BT keyboard implementation and is copied from https://gist.github.com/manuelbl/66f059effc8a7be148adb1f104666467  
+
+// Message (report) sent when a key is pressed or released
+struct InputReport {
+    uint8_t modifiers;	     // bitmask: CTRL = 1, SHIFT = 2, ALT = 4
+    uint8_t reserved;        // must be 0
+    uint8_t pressedKeys[6];  // up to six concurrenlty pressed keys
+};
+
+// Message (report) received when an LED's state changed
+struct OutputReport {
+    uint8_t leds;            // bitmask: num lock = 1, caps lock = 2, scroll lock = 4, compose = 8, kana = 16
+};
+
+
+// The report map describes the HID device (a keyboard in this case) and
+// the messages (reports in HID terms) sent and received.
+static const uint8_t REPORT_MAP[] = {
+    USAGE_PAGE(1),      0x01,       // Generic Desktop Controls
+    USAGE(1),           0x06,       // Keyboard
+    COLLECTION(1),      0x01,       // Application
+    REPORT_ID(1),       0x01,       //   Report ID (1)
+    USAGE_PAGE(1),      0x07,       //   Keyboard/Keypad
+    USAGE_MINIMUM(1),   0xE0,       //   Keyboard Left Control
+    USAGE_MAXIMUM(1),   0xE7,       //   Keyboard Right Control
+    LOGICAL_MINIMUM(1), 0x00,       //   Each bit is either 0 or 1
+    LOGICAL_MAXIMUM(1), 0x01,
+    REPORT_COUNT(1),    0x08,       //   8 bits for the modifier keys
+    REPORT_SIZE(1),     0x01,       
+    HIDINPUT(1),        0x02,       //   Data, Var, Abs
+    REPORT_COUNT(1),    0x01,       //   1 byte (unused)
+    REPORT_SIZE(1),     0x08,
+    HIDINPUT(1),        0x01,       //   Const, Array, Abs
+    REPORT_COUNT(1),    0x06,       //   6 bytes (for up to 6 concurrently pressed keys)
+    REPORT_SIZE(1),     0x08,
+    LOGICAL_MINIMUM(1), 0x00,
+    LOGICAL_MAXIMUM(1), 0x65,       //   101 keys
+    USAGE_MINIMUM(1),   0x00,
+    USAGE_MAXIMUM(1),   0x65,
+    HIDINPUT(1),        0x00,       //   Data, Array, Abs
+    REPORT_COUNT(1),    0x05,       //   5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
+    REPORT_SIZE(1),     0x01,
+    USAGE_PAGE(1),      0x08,       //   LEDs
+    USAGE_MINIMUM(1),   0x01,       //   Num Lock
+    USAGE_MAXIMUM(1),   0x05,       //   Kana
+    LOGICAL_MINIMUM(1), 0x00,
+    LOGICAL_MAXIMUM(1), 0x01,
+    HIDOUTPUT(1),       0x02,       //   Data, Var, Abs
+    REPORT_COUNT(1),    0x01,       //   3 bits (Padding)
+    REPORT_SIZE(1),     0x03,
+    HIDOUTPUT(1),       0x01,       //   Const, Array, Abs
+    END_COLLECTION(0)               // End application collection
+};
+
+
+BLEHIDDevice* hid;
+BLECharacteristic* input;
+BLECharacteristic* output;
+
+const InputReport NO_KEY_PRESSED = { };
+
+
+/*
+ * Callbacks related to BLE connection
+ */
+class BleKeyboardCallbacks : public BLEServerCallbacks {
+
+    void onConnect(BLEServer* server) {
+        isBleConnected = true;
+
+        // Allow notifications for characteristics
+        BLE2902* cccDesc = (BLE2902*)input->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+        cccDesc->setNotifications(true);
+
+        Serial.println("Client has connected");
+    }
+
+    void onDisconnect(BLEServer* server) {
+        isBleConnected = false;
+
+        // Disallow notifications for characteristics
+        BLE2902* cccDesc = (BLE2902*)input->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+        cccDesc->setNotifications(false);
+
+        Serial.println("Client has disconnected - Wait 5 Seconds");
+      // Should probably do a reset when this happens?
+      delay(5000);
+      resetFunc();
+    }
+};
+
+
+/*
+ * Called when the client (computer, smart phone) wants to turn on or off
+ * the LEDs in the keyboard.
+ * 
+ * bit 0 - NUM LOCK
+ * bit 1 - CAPS LOCK
+ * bit 2 - SCROLL LOCK
+ */
+ class OutputCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* characteristic) {
+        OutputReport* report = (OutputReport*) characteristic->getData();
+        Serial.print("LED state: ");
+        Serial.print((int) report->leds);
+        Serial.println();
+    }
+};
+
+
+void bluetoothTask(void*) {
+
+    // initialize the device
+    BLEDevice::init(DEVICE_NAME);
+    BLEServer* server = BLEDevice::createServer();
+    server->setCallbacks(new BleKeyboardCallbacks());
+
+    // create an HID device
+    hid = new BLEHIDDevice(server);
+    input = hid->inputReport(1); // report ID
+    output = hid->outputReport(1); // report ID
+    output->setCallbacks(new OutputCallbacks());
+
+    // set manufacturer name
+    hid->manufacturer()->setValue("Maker Community");
+    // set USB vendor and product ID
+    hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
+    // information about HID device: device is not localized, device can be connected
+    hid->hidInfo(0x00, 0x02);
+
+    // Security: device requires bonding
+    BLESecurity* security = new BLESecurity();
+    security->setAuthenticationMode(ESP_LE_AUTH_BOND);
+
+    // set report map
+    hid->reportMap((uint8_t*)REPORT_MAP, sizeof(REPORT_MAP));
+    hid->startServices();
+
+    // set battery level to 100%
+    hid->setBatteryLevel(100);
+
+    // advertise the services
+    BLEAdvertising* advertising = server->getAdvertising();
+    advertising->setAppearance(HID_KEYBOARD);
+    advertising->addServiceUUID(hid->hidService()->getUUID());
+    advertising->addServiceUUID(hid->deviceInfo()->getUUID());
+    advertising->addServiceUUID(hid->batteryService()->getUUID());
+    advertising->start();
+
+    Serial.println("BLE ready");
+    delay(portMAX_DELAY);
+};
+
+
+void typeText(const char* text) {
+    int len = strlen(text);
+    for (int i = 0; i < len; i++) {
+
+        // translate character to key combination
+        uint8_t val = (uint8_t)text[i];
+        if (val > KEYMAP_SIZE)
+            continue; // character not available on keyboard - skip
+        KEYMAP map = keymap[val];
+
+        // create input report
+        InputReport report = {
+            .modifiers = map.modifier,
+            .reserved = 0,
+            .pressedKeys = {
+                map.usage,
+                0, 0, 0, 0, 0
+            }
+        };
+
+        // send the input report
+        input->setValue((uint8_t*)&report, sizeof(report));
+        input->notify();
+
+        delay(5);
+
+        // release all keys between two characters; otherwise two identical
+        // consecutive characters are treated as just one key press
+        input->setValue((uint8_t*)&NO_KEY_PRESSED, sizeof(NO_KEY_PRESSED));
+        input->notify();
+
+        delay(5);
+    }
+}
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
